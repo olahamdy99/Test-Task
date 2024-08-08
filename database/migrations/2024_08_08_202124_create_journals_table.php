9c0aa6fd0d8da6370417199c0f431a3e049a230a
Ola Hamdy <olahamdy139@gmail.om>
2024-08-08 23:24:37 +0300

    Create journal model and  migration

---
+++ database/migrations/2024_08_08_202124_create_journals_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('journals', function (Blueprint $table) {
+            $table->id();
+            $table->float('amount');
+            $table->enum('type', ['Debit Cash', 'Credit Revenue', 'Credit Payable']);
+            $table->foreignId('shipment_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('journals');
+    }
+};
